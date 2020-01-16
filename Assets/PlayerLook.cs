f5b9fb3e2c5036f99afdd7e208e6e1d177f08d50
Leopold Cramer <2lcramer@gmail.com>
2020-01-16 16:41:43 +0100

    Importing base project

---
+++ Assets/PlayerLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    [SerializeField] Transform _playerBody;
+
+    [SerializeField] float _mouseSensitivity = 100f;
+    
+
+    float _xRotation = 0f;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody _rig;
+
+    [SerializeField] float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        float yMove;
+        float zMove = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+
+        _rig.velocity = new Vector3(xMove, _rig.velocity.y, zMove);
+    }
+}
