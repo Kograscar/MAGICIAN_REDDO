2c8ed03cd1dd55e1b5994f808c501d8769edaa0b
Leopold Cramer <2lcramer@gmail.com>
2020-01-30 13:25:27 +0100

    je c pa

---
+++ Assets/PauvreFou.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class PauvreFou
+{
+    private static string currentScene;
+    static PauvreFou()
+    {
+        currentScene = EditorApplication.currentScene;
+        EditorApplication.hierarchyWindowChanged += hierarchyWindowChanged;
+    }
+    private static void hierarchyWindowChanged()
+    {
+        if (currentScene != EditorApplication.currentScene)
+        {
+            //a scene change has happened
+            Debug.Log("Last Scene: " + currentScene);
+            currentScene = EditorApplication.currentScene;
+        }
+    }
+}
